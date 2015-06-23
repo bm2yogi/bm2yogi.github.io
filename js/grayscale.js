e4d1aeb81ba95931737859e4f36089552e1ea6b7
Michael Ibarra <mibarra@gmail.com>
2015-06-23 00:07:22 -0700

    add signup form

--- js/grayscale.js
+++ js/grayscale.js
@@ -10,6 +10,7 @@ $(window).scroll(function() {
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
+        console.log('11');
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
