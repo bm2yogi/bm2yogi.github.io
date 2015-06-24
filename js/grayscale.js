2d08ee84cc89d21f333d798af3624e0d82333509
Michael Ibarra <mibarra@gmail.com>
2015-06-23 23:05:49 -0700

    fixed navbar.

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,16 +1,15 @@
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+   /* if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+    }*/
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
-        console.log('11');
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
